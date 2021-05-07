6c25da1de0473d1beedfb9488b5a77be7aae3fb4
Ovikt0r <viktor.oblapenko@gmail.com>
2021-05-08 00:48:05 +0300

    Add a few more

---
+++ src/com/company/oviktor/bookexamples/AutoInc.java
@@ -0,0 +1,16 @@
+//: operators/AutoInc.java
+// Demonstrates the ++ and -- operators.
+package com.company.oviktor.bookexamples;
+
+public class AutoInc {
+    public static void main(String[] args) {
+        int i = 1;
+        System.out.println("i : " + i);
+        System.out.println("++i : " + ++i);
+        System.out.println("i++ : " + i++);
+        System.out.println("i : " + i);
+        System.out.println("--i : " + --i);
+        System.out.println("i-- : " + i--);
+        System.out.println("i : " + i);
+    }
+}
