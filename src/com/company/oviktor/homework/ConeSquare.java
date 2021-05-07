864539c9bb816c9c31efb45a9b8faa12d43a5f60
Ovikt0r <viktor.oblapenko@gmail.com>
2021-05-08 00:48:05 +0300

    The changes of the project structure

--- src/com/company/oviktor/homework/ConeSquare.java
+++ src/com/company/oviktor/homework/ConeSquare.java
@@ -10,20 +10,17 @@ public class ConeSquare {
     static double l;
     static double s;
     static char ch;
-    static Scanner in = new Scanner(System.in);
 
     public static void main(String[] args) {
-
         System.out.println("Hi! This program calculate whole surface area of the cone.");
         System.out.println("You should enter the radius 'r' and the height 'h' to within two decimal places");
         System.out.println("Enter the cone base radius: ");
         calculateSquare();
-
     }
 
     static void calculateSquare() {
 
-        try {
+        try (Scanner in = new Scanner(System.in)) {
             System.out.println("r:");
             while (!in.hasNextDouble()) {
                 System.out.println("Enter the number in the right format !");
@@ -38,7 +35,6 @@ public class ConeSquare {
                 System.out.println("h:");
                 in.next();
             }
-
             h = in.nextDouble();
             if (r > 0 && h > 0) {
                 l = Math.sqrt(Math.pow(r, 2) + Math.pow(h, 2));
@@ -51,33 +47,28 @@ public class ConeSquare {
                 System.err.println("The value cannot be calculated! Enter positive number, please");
             }
             continueCalculate();
-
-        } catch (
-                Exception e) {
-            String bad_input = in.next();
-            System.out.println("You entered something wrong : " + bad_input);
         }
     }
-
 
     static void continueCalculate() {
 
         System.out.println("Do you want to calculate the square again ? Press Y/N");
-        while (in.hasNextDouble()) {
-            System.out.println("Enter the right alphabetical symbol !");
-            in.next();
-        }
-        ch = in.next(".").charAt(0);
+        try (Scanner in = new Scanner(System.in)) {
+            while (in.hasNextDouble()) {
+                System.out.println("Enter the right alphabetical symbol !");
+                in.next();
+            }
+            ch = in.next(".").charAt(0);
 
-        if (ch != 'Y' && ch != 'y' && ch != 'N' && ch != 'n') {
-            System.out.println("Enter the right alphabetical symbol !");
-            in.next();
-        } else if (ch == 'Y' || ch == 'y') {
-            calculateSquare();
-        } else {
-            System.out.println("Thank you. Good bye!");
+            if (ch != 'Y' && ch != 'y' && ch != 'N' && ch != 'n') {
+                System.out.println("Enter the right alphabetical symbol !");
+                in.next();
+            } else if (ch == 'Y' || ch == 'y') {
+                calculateSquare();
+            } else {
+                System.out.println("Thank you. Good bye!");
+            }
         }
+
     }
-
-
 }
--- src/com/company/oviktor/homework/DaysOfWeek.java
+++ src/com/company/oviktor/homework/DaysOfWeek.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 class DaysOfWeek {
     static int item;
     static char ch;
-    static Scanner in = new Scanner(System.in);
 
     public static void main(String[] args) {
         calculateDay();
@@ -13,14 +12,13 @@ class DaysOfWeek {
 
     static void calculateDay() {
 
-        try {
+        try (Scanner in = new Scanner(System.in))  {
             System.out.println("Enter the number of the day :");
             while (!in.hasNextInt()) {
                 System.out.println("Enter the number in the range from 1 to 7, please!");
                 in.next();
             }
             item = in.nextInt();
-
             if (item > 0 && item < 8) {
                 switch (item) {
                     case 1 -> System.out.println("Monday");
@@ -36,30 +34,27 @@ class DaysOfWeek {
                 System.out.println("Enter the number in the range from 1 to 7, please!");
                 continueCalculate();
             }
-
-        } catch (Exception e) {
-            String bad_input = in.next();
-            System.out.println("You entered something wrong : " + bad_input);
         }
     }
 
     static void continueCalculate() {
 
         System.out.println("Do you want to calculate the square again ? Press Y/N");
-        while (in.hasNextDouble()) {
-            System.out.println("Don't enter numbers !");
-            in.next();
-        }
-        ch = in.next(".").charAt(0);
+        try (Scanner in = new Scanner(System.in)) {
+            while (in.hasNextDouble()) {
+                System.out.println("Don't enter numbers !");
+                in.next();
+            }
+            ch = in.next(".").charAt(0);
 
-        if (ch != 'Y' && ch != 'y' && ch != 'N' && ch != 'n') {
-            System.out.println("Enter the right alphabetical symbol !");
-            in.next();
-
-        } else if (ch == 'Y' || ch == 'y') {
-            calculateDay();
-        } else {
-            System.out.println("Thank you. Good bye!");
+            if (ch != 'Y' && ch != 'y' && ch != 'N' && ch != 'n') {
+                System.out.println("Enter the right alphabetical symbol !");
+                in.next();
+            } else if (ch == 'Y' || ch == 'y') {
+                calculateDay();
+            } else {
+                System.out.println("Thank you. Good bye!");
+            }
         }
     }
 }
