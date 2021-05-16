36fe82908b13085784b7bcf9dfe4edc5c479281f
Ovikt0r <viktor.oblapenko@gmail.com>
2021-05-16 22:49:46 +0300

    Assigment.java without Print class

--- src/com/company/oviktor/bookexamples/Assigment.java
+++ src/com/company/oviktor/bookexamples/Assigment.java
@@ -1,7 +1,5 @@
 package com.company.oviktor.bookexamples;
 
-import com.company.oviktor.tools.*;
-
 class Tank {
     int level;
 
@@ -15,12 +13,11 @@ public class Assigment {
         t2 = new Tank();
         t1.level = 9;
         t2.level = 47;
-        System.out.println(" 1: t1.level: " + t1.level + ", t2.level: " + t2.level+ "\n");
+        System.out.println("1: t1.level: " + t1.level + ", t2.level: " + t2.level);
         t1=t2;
-        System.out.println(" 2: t1.level: " + t1.level + ", t2.level: " + t2.level+ "\n");
+        System.out.println("2: t1.level: " + t1.level + ", t2.level: " + t2.level);
         t1.level = 27;
-        System.out.println(" 3: t1.level: " + t1.level + ", t2.level: " + t2.level+ "\n");
+        System.out.println("3: t1.level: " + t1.level + ", t2.level: " + t2.level);
 
     }
-    }
-
+}
