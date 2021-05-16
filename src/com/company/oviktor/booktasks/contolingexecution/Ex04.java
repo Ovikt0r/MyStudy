651a276a9e81a56f5779233c805bc46333b0d93c
Ovikt0r <viktor.oblapenko@gmail.com>
2021-05-16 23:03:29 +0300

    initial commit with solved 4th task

---
+++ src/com/company/oviktor/booktasks/contolingexecution/Ex04.java
@@ -0,0 +1,22 @@
+package com.company.oviktor.booktasks.contolingexecution;
+
+public class Ex04 {
+
+    public static void main(String[] args) {
+
+        int max = 100;
+
+        for (int i=2; i<max; i++) {
+            boolean next = true;
+            for(int j=2; j<i; j++) {
+                if (i % j == 0) {
+                    next = false;
+                    break;
+                }
+            }
+            if (next)
+                System.out.print(i + " ");
+        }
+    }
+
+}
