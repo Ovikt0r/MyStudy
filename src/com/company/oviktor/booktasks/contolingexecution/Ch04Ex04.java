1211a62036c17c1bcb6d72004509c8bd7f2e0726
Ovikt0r <viktor.oblapenko@gmail.com>
2021-05-25 12:44:26 +0300

    initial commit with solved 7th task

--- src/com/company/oviktor/booktasks/contolingexecution/Ch04Ex04.java
+++ src/com/company/oviktor/booktasks/contolingexecution/Ch04Ex04.java
@@ -6,15 +6,17 @@ public class Ch04Ex04 {
 
         int max = 100;
 
-        for (int i=1; i<max; i++) {
+        for (int i=2; i<max; i++) {
             boolean next = true;
-            for(int j=2; j<i; j++){
-                if (i % j==0) {
+            for(int j=2; j<i; j++) {
+                if (i % j == 0) {
                     next = false;
-                } else if (next)
-                    System.out.print(i + " ");
+                    break;
+                }
+            }
+             if (next)
+                 System.out.print(i + " ");
             }
         }
 
     }
-}
---
+++ src/com/company/oviktor/booktasks/contolingexecution/Ch04Ex05.java
@@ -0,0 +1,16 @@
+package com.company.oviktor.booktasks.contolingexecution;
+
+public class Ch04Ex05 {
+
+    static boolean test(int test, int begin, int end) {
+        return test >= begin && test <= end;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(test(5, 4,20));
+        System.out.println(test(90, 5,20));
+        System.out.println(test(4, 4,21));
+        System.out.println(test(20, 4,20));
+
+    }
+}
