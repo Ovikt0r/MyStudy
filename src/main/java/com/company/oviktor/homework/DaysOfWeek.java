28f58b28debb41da3ff33fb92d46a7a8fd0e1527
Viktor Oblapenko <83182493+Ovikt0r@users.noreply.github.com>
2021-06-05 23:48:40 +0300

    Set up logging with slf4j and logback, fix 2 task

--- src/main/java/com/company/oviktor/homework/DaysOfWeek.java
+++ src/main/java/com/company/oviktor/homework/DaysOfWeek.java
@@ -1,36 +1,41 @@
 package com.company.oviktor.homework;
 
 import java.util.Scanner;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 class DaysOfWeek {
 
+    private final static Logger log = LoggerFactory.getLogger(DaysOfWeek.class);
+
 
     public static void main(String[] args) {
+        log.info("It's a launch of the app");
         calculateDay();
     }
 
     static void calculateDay() {
 
         try (Scanner in = new Scanner(System.in)) {
-            System.out.println("Enter the number of the day :");
+            log.info("Enter the number of the day :");
             while (!in.hasNextInt()) {
-                System.out.println("Enter the number in the range from 1 to 7, please!");
+                log.info("Enter the number in the range from 1 to 7, please!");
                 in.next();
             }
             int item = in.nextInt();
             if (item > 0 && item < 8) {
                 switch (item) {
-                    case 1 -> System.out.println("Monday");
-                    case 2 -> System.out.println("Tuesday");
-                    case 3 -> System.out.println("Wednesday");
-                    case 4 -> System.out.println("Thursday");
-                    case 5 -> System.out.println("Friday");
-                    case 6 -> System.out.println("Saturday");
-                    case 7 -> System.out.println("Sunday");
+                    case 1 -> log.info("Monday");
+                    case 2 -> log.info("Tuesday");
+                    case 3 -> log.info("Wednesday");
+                    case 4 -> log.info("Thursday");
+                    case 5 -> log.info("Friday");
+                    case 6 -> log.info("Saturday");
+                    case 7 -> log.info("Sunday");
                 }
                 continueCalculate();
             } else {
-                System.out.println("Enter the number in the range from 1 to 7, please!");
+                log.info("Enter the number in the range from 1 to 7, please!");
                 continueCalculate();
             }
         }
@@ -38,7 +43,7 @@ class DaysOfWeek {
 
     static void continueCalculate() {
 
-        System.out.println("Do you want to calculate the square again ? Type 'Yes' or 'No'");
+        log.info("Do you want to calculate the square again ? Type 'Yes' or 'No'");
         try (Scanner in = new Scanner(System.in)) {
             while (true) {
                 String input = in.nextLine();
@@ -46,11 +51,11 @@ class DaysOfWeek {
                     calculateDay();
                     break;
                 } else if (input.equalsIgnoreCase("no")) {
-                    System.out.println("Good bye");
+                    log.info("Good bye");
                     break;
                 } else {
-                    System.out.println("Make the right choice !");
-                    System.out.println("Type 'Yes' or 'No'");
+                    log.info("Make the right choice !");
+                    log.info("Type 'Yes' or 'No'");
                 }
             }
         }
