f221cf74372c6224aa79fa2f0971481226919231
Ovikt0r <viktor.oblapenko@gmail.com>
2021-06-12 19:37:52 +0300

    Chapter05 Ex04

--- src/main/java/com/company/oviktor/booktasks/initializationcleanup/Ex04.java
+++ src/main/java/com/company/oviktor/booktasks/initializationcleanup/Ex04.java
@@ -2,7 +2,16 @@ package com.company.oviktor.booktasks.initializationcleanup;
 
 public class Ex04 {
 
+    Ex04(){
+        System.out.println("It's the message of the class");
+    }
+
+    Ex04(String s){
+        System.out.println(s);
+    }
+
     public static void main(String[] args) {
-        System.out.println("sdfr");
+        new Ex04();
+        new Ex04("bla-bla-bla, it's overloading constructor");
     }
 }
