d7ff57355f76f31bd2da018cd0d59553a14c2463
Ovikt0r <viktor.oblapenko@gmail.com>
2021-07-21 21:42:34 +0300

    added rle task

---
+++ src/main/java/com/company/oviktor/homework/RunLengthEncoding.java
@@ -0,0 +1,27 @@
+package com.company.oviktor.homework;
+
+public class RunLengthEncoding {
+
+    public void encode (String str) {
+        int counter;
+        StringBuilder output = new StringBuilder();
+
+        for(int i = 0; i < str.length(); i++) {
+            counter = 1;
+            while (i < str.length() - 1  && str.charAt(i) == str.charAt(i+1) ) {
+                counter++;
+                i++;
+            }
+            output.append(str.charAt(i)).append(counter);
+
+        }
+        System.out.println("Input string is : " + str);
+        System.out.println("Output string is : " + output);
+        System.out.println("Difference is : " + (str.length() - output.length()));
+
+    }
+
+    public static void main(String[] args) {
+        new RunLengthEncoding().encode("AAAAbbCCCCCCppphffHydLLLLLLLBBBc");
+    }
+}
