5ac8cdf1345b516cbf584c7d0bb15a59332a6011
Ovikt0r <viktor.oblapenko@gmail.com>
2021-07-22 22:48:36 +0300

    added Ex01 from chapter 06
    added Ex01 from chapter 06

---
+++ src/main/java/com/company/oviktor/booktasks/accesscontrol/Ex01p2.java
@@ -0,0 +1,10 @@
+package com.company.oviktor.booktasks.accesscontrol;
+
+import com.company.oviktor.booktasks.accesscontrol.inpackage.Ex01p1;
+
+public class Ex01p2 {
+
+    public static void main(String[] args) {
+        new Ex01p1(6);
+    }
+}
---
+++ src/main/java/com/company/oviktor/booktasks/accesscontrol/inpackage/Ex01p1.java
@@ -0,0 +1,8 @@
+package com.company.oviktor.booktasks.accesscontrol.inpackage;
+
+public class Ex01p1 {
+    int i;
+    public Ex01p1(int i){
+        this.i = i;
+    }
+}
