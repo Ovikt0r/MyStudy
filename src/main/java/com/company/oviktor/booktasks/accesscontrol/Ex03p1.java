97601de07efe934f248bcbe609c020752456a441
Ovikt0r <viktor.oblapenko@gmail.com>
2021-07-22 23:42:10 +0300

    added Ex03

---
+++ src/main/java/com/company/oviktor/booktasks/accesscontrol/Ex03p1.java
@@ -0,0 +1,9 @@
+package com.company.oviktor.booktasks.accesscontrol;
+import static com.company.oviktor.booktasks.accesscontrol.debug.Ex03.*;
+
+public class Ex03p1 {
+
+    public static void main(String[] args) {
+        debug("my message to you");
+    }
+}
---
+++ src/main/java/com/company/oviktor/booktasks/accesscontrol/debugoff/Ex03.java
@@ -0,0 +1,8 @@
+package com.company.oviktor.booktasks.accesscontrol.debugoff;
+
+public class Ex03 {
+
+    public static void debug(String text) {
+
+    }
+}
