6abe1644c94ff3a71f5cedcbf3caa1f48ff7bfc5
Ovikt0r <viktor.oblapenko@gmail.com>
2021-08-12 23:30:59 +0300

    Added Ex 08

---
+++ src/main/java/com/company/oviktor/booktasks/accesscontrol/Ex08_ConnectionManager.java
@@ -0,0 +1,56 @@
+package com.company.oviktor.booktasks.accesscontrol;
+
+class Connection {
+
+    private static int count = 1;
+    private int id = count++;
+    private Connection () {}
+    static Connection makeConnection() {
+        return new Connection();
+    }
+    public String toString(){
+        return "Connection " + id;
+    }
+
+    public void doSomething() {
+        System.out.println("It is how works some method invocation from makeConnection method");
+    }
+}
+
+public class Ex08_ConnectionManager {
+
+    static int howManyLeft = 5;
+    static Connection[] c = new Connection[howManyLeft];
+
+    public static Connection getConnection(){
+        if(howManyLeft > 0)
+            return c[--howManyLeft];
+        else {
+            System.out.println("Mo more connections");
+            return null;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Connection.makeConnection());
+        System.out.println(Connection.makeConnection());
+        System.out.println(Connection.makeConnection());
+        System.out.println(Connection.makeConnection());
+        System.out.println(Connection.makeConnection());
+        Connection.makeConnection().doSomething();
+
+        System.out.println(howManyLeft);
+        getConnection();
+        System.out.println(howManyLeft);
+        getConnection();
+        System.out.println(howManyLeft);
+        getConnection();
+        System.out.println(howManyLeft);
+        getConnection();
+        System.out.println(howManyLeft);
+        getConnection();
+        System.out.println(getConnection());
+
+        }
+    }
+
