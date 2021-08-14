b107e01b801b3c9e5be919849a24c01abee23436
Ovikt0r <viktor.oblapenko@gmail.com>
2021-08-14 19:37:47 +0300

    Added Ex 09

---
+++ src/main/java/com/company/oviktor/booktasks/accesscontrol/foreign/Ex09_Foreign.java
@@ -0,0 +1,10 @@
+package com.company.oviktor.booktasks.accesscontrol.foreign;
+import com.company.oviktor.booktasks.accesscontrol.local.*;
+
+public class Ex09_Foreign {
+
+    public static void main(String[] args) {
+        Ex09_Local local = new Ex09_Local();
+
+    }
+}
