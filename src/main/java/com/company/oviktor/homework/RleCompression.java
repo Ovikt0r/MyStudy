be05c0b9c3fb4c6d16c6ca74fca13af73290038d
Ovikt0r <viktor.oblapenko@gmail.com>
2021-08-20 17:06:08 +0300

    Initial commit first variant solving rle-task

--- src/main/java/com/company/oviktor/homework/RleCompression.java
+++ src/main/java/com/company/oviktor/homework/RleCompression.java
@@ -3,27 +3,51 @@ package com.company.oviktor.homework;
 public class RleCompression {
 
     public String encode(String input) {
-        StringBuilder result = new StringBuilder();
-
+        StringBuilder resultIn = new StringBuilder();
 
         for (int i = 0; i < input.length(); i++) {
             int count = 1;
             while (i < input.length() - 1 && input.charAt(i) == input.charAt(i + 1)) {
                 count++;
                 i++;
-
             }
-
-            result.append(input.charAt(i)).append(count);
-
-
+            resultIn.append(input.charAt(i)).append(count);
         }
-        return result.toString();
+        return resultIn.toString();
+    }
+
+    public String decode(String output) {
+        StringBuilder resultOut = new StringBuilder();
+
+        for (int i = 0; i < output.length(); i += 2) {
+            if (Character.isAlphabetic(output.charAt(i)) && Character.isDigit(output.charAt(i + 1))) {
+                char c = output.charAt(i);
+                resultOut.append(String.valueOf(c).repeat(output.charAt(i + 1) - '0'));
+            } else {
+                System.out.println("The string has incorrect sequences of characters !");
+            }
+        }
+        return resultOut.toString();
+    }
+
+    public String decode2 (String output) {
+        StringBuilder resultOut = new StringBuilder();
+
+        for (int i = 0; i < output.length(); i += 2) {
+            if (Character.isAlphabetic(output.charAt(i)) && Character.isDigit(output.charAt(i + 1))) {
+                char c = output.charAt(i);
+                resultOut.append(String.valueOf(c).repeat(output.charAt(i + 1) - '0'));
+            } else {
+                System.out.println("The string has incorrect sequences of characters !");
+            }
+        }
+        return resultOut.toString();
     }
 
     public static void main(String[] args) {
 
         RleCompression rle = new RleCompression();
-        System.out.println(rle.encode("AAbbM"));
+        System.out.println(rle.encode("AAAAAAAAAAAAAAAbbM"));
+        System.out.println(rle.decode("A15B5J3l6v1"));
     }
 }
