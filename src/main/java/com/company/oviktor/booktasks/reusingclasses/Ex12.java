ee1192428a5f0ed82ce6777b4ded75c7b7fd70fc
Ovikt0r <viktor.oblapenko@gmail.com>
2021-08-22 13:51:03 +0300

    Initial commit Adding Ex12

--- src/main/java/com/company/oviktor/booktasks/reusingclasses/Ex12.java
+++ src/main/java/com/company/oviktor/booktasks/reusingclasses/Ex12.java
@@ -4,11 +4,17 @@ class Component1 {
     Component1(){
         System.out.println("Calling Component1 ");
     }
+    void dispose(){
+        System.out.println("Component1 dispose");
+    }
 }
 
 class Component2 {
     Component2(){
         System.out.println("Calling Component2");
+    }
+    void dispose(){
+        System.out.println("Component2 dispose");
     }
 }
 
@@ -16,7 +22,11 @@ class Component3 {
     Component3(){
         System.out.println("Calling Component3");
     }
+    void dispose(){
+        System.out.println("Component3 dispose");
+    }
 }
+
 
 class Root {
     Component1 c1 = new Component1();
@@ -25,19 +35,32 @@ class Root {
     Root(){
         System.out.println("Calling Root");
     }
-}
-
-class Stem extends Root{
-    Component1 c1 = new Component1();
-    Component2 c2 = new Component2();
-    Component3 c3 = new Component3();
-    Stem() {
-        System.out.println("Calling Stem");
+    void dispose(){
+        System.out.println("Root dispose");
     }
 }
 
-public class Ex09 {
+class Stem extends Root{
+    Stem() {
+        System.out.println("Calling Stem");
+    }
+    void dispose(){
+        System.out.println("Stem dispose");
+        super.dispose();
+    }
+}
+
+public class Ex12 {
     public static void main(String[] args) {
-        new Stem();
+        Stem stem = new Stem();
+        try {
+            System.out.println("Try area is done");
+        }
+        finally {
+            stem.c1.dispose();
+            stem.c2.dispose();
+            stem.c3.dispose();
+            stem.dispose();
+        }
     }
 }
