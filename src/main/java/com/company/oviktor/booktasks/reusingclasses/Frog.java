d36db859880c7a72ba05a8eeed6c51400bfa2cbf
Ovikt0r <viktor.oblapenko@gmail.com>
2021-08-31 22:59:04 +0300

    Initial commit editing pom file

--- src/main/java/com/company/oviktor/booktasks/reusingclasses/Frog.java
+++
@@ -1,8 +0,0 @@
-package com.company.oviktor.booktasks.reusingclasses;
-
-public class Frog extends Amphibian{
-
-    void squawk(){
-        System.out.println("I'm a fog. I'm squawking");
-    }
-}
