5984b1b41e132613b314ca87eceb1aa7ffb0b83c
Ovikt0r <viktor.oblapenko@gmail.com>
2021-09-04 21:05:52 +0300

    Initial commit added ex22

---
+++ src/main/java/com/company/oviktor/booktasks/reusingclasses/Ex22.java
@@ -0,0 +1,14 @@
+package com.company.oviktor.booktasks.reusingclasses;
+
+
+final class BaseClass {
+
+}
+
+
+public class Ex22 extends BaseClass{
+
+    public static void main(String[] args) {
+
+    }
+}
