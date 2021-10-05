12f6527603bc6f6bffaaef417d081293664d4e7c
Ovikt0r <viktor.oblapenko@gmail.com>
2021-10-05 22:01:02 +0300

    Initial commit added ex13

---
+++ src/main/java/com/company/oviktor/booktasks/polimorphism/Ex13_ReferenceCounting.java
@@ -0,0 +1,43 @@
+package com.company.oviktor.booktasks.polimorphism;
+
+class Shared {
+    private int refcount = 0;
+    private static long counter = 0;
+    private final long id = counter++;
+    public Shared() {
+        System.out.println("Create " + this);
+    }
+    public void addRef() { refcount++;}
+    protected void dispose() {
+        if(--refcount == 0)
+            System.out.println("Disposing " + this);
+    }
+    public String toString() {return "Shared " + id;}
+}
+
+class Composing {
+    private Shared shared;
+    private static long counter = 0;
+    private final long id = counter++;
+    public Composing(Shared shared) {
+        System.out.println("Create " + this);
+        this.shared = shared;
+        this.shared.addRef();
+    }
+    protected void dispose() {
+        System.out.println("disposing " + this);
+        shared.dispose();
+    }
+    public String toString() {return "Composing " + id;}
+}
+
+public class Ex13_ReferenceCounting {
+    public static void main(String[] args) {
+        Shared shared = new Shared();
+        Composing[] composings = {new Composing(shared),
+                new Composing(shared), new Composing(shared),
+                new Composing(shared),new Composing(shared)};
+        for (Composing c:composings)
+            c.dispose();
+    }
+}
