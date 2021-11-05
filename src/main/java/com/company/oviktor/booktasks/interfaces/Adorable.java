b46c26b5ae5e48e395599076149fb60a0dc69c0d
Ovikt0r <viktor.oblapenko@gmail.com>
2021-11-05 23:48:18 +0200

    Initial commit added ex13

---
+++ src/main/java/com/company/oviktor/booktasks/interfaces/Adorable.java
@@ -0,0 +1,11 @@
+package com.company.oviktor.booktasks.interfaces;
+
+public interface Adorable {
+
+    void makeLove();
+
+    void noWar();
+
+    void cryBaby();
+
+}
---
+++ src/main/java/com/company/oviktor/booktasks/interfaces/Cryable.java
@@ -0,0 +1,6 @@
+package com.company.oviktor.booktasks.interfaces;
+
+public interface Cryable extends Adorable {
+
+    void dropTears();
+}
