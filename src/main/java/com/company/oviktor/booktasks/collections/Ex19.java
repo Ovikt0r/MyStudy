5004f120243e50015c5c7344f586708e4b71bdfb
Ovikt0r <viktor.oblapenko@gmail.com>
2022-02-10 23:09:10 +0200

    Initial commit adding exercise Ex19

--- src/main/java/com/company/oviktor/booktasks/collections/Ex19.java
+++ src/main/java/com/company/oviktor/booktasks/collections/Ex19.java
@@ -5,7 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import java.util.*;
 
 @Slf4j
-public class Ex16 {
+public class Ex19 {
 
     private final static Set<Character> vowels = new HashSet<>(Arrays.asList('a', 'e', 'o', 'u',
             'i', 'A', 'E', 'O', 'U', 'I'));
