cd4f1c5bdc0d545e76b6ca2639e93bd15044d005
Ovikt0r <viktor.oblapenko@gmail.com>
2022-02-10 22:42:36 +0200

    Initial commit adding exercise Ex19

--- src/main/java/com/company/oviktor/booktasks/collections/Ex16.java
+++ src/main/java/com/company/oviktor/booktasks/collections/Ex16.java
@@ -9,29 +9,31 @@ public class Ex16 {
 
     private final static Set<Character> vowels = new HashSet<>(Arrays.asList('a', 'e', 'o', 'u',
             'i', 'A', 'E', 'O', 'U', 'I'));
-    private static final Map<Character, Integer> vowelAmount = new HashMap<>();
+    private static final Map<Character, Integer> vowelAmount = new TreeMap<>();
+    private static final Map<Character, Integer> fileAmount = new TreeMap<>();
+
+    public static void updateStat(Map<Character, Integer> map, char ch) {
+        Integer i = map.get(ch);
+        map.put(ch, i == null ? 1 : i+1);
+    }
+
 
     public static void main(String[] args) {
         HashSet<String> processWords = new HashSet<>();
-        int fileVowels = 0;
-        int wordVowels;
 
         for (String word : new TextFile("text.txt", "\\W+")) {
-            wordVowels = 0;
-            for (char letter : word.toCharArray()) {
+            vowelAmount.clear();
+            for (char letter : word.toCharArray())
                 if (vowels.contains(letter)) {
-                    wordVowels++;
-                    vowelAmount.put(letter, !vowelAmount.containsKey(letter) ? 1 : vowelAmount.get(letter) + 1);
+                    updateStat(vowelAmount, letter);
+                    updateStat(fileAmount, letter);
                 }
-
-            }
             if (!processWords.contains(word)) {
                 processWords.add(word);
-                log.info(word + " has " + wordVowels + " vowel(s)");
+                log.info("Vowel(s) in " + word + " : " + vowelAmount);
             }
-            fileVowels += wordVowels;
         }
-        log.info("Total number vowels in the file are " + fileVowels);
-        log.info(vowelAmount.toString());
+
+        log.info(fileAmount.toString());
     }
 }
