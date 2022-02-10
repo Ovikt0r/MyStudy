a11e6135360c90f36214bcc577cfd0d8e18e8a42
Ovikt0r <viktor.oblapenko@gmail.com>
2022-02-10 22:14:58 +0200

    Initial commit adding exercise Ex19

--- src/main/java/com/company/oviktor/booktasks/collections/Ex16.java
+++ src/main/java/com/company/oviktor/booktasks/collections/Ex16.java
@@ -2,18 +2,17 @@ package com.company.oviktor.booktasks.collections;
 
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 @Slf4j
 public class Ex16 {
 
     private final static Set<Character> vowels = new HashSet<>(Arrays.asList('a', 'e', 'o', 'u',
             'i', 'A', 'E', 'O', 'U', 'I'));
+    private static final Map<Character, Integer> vowelAmount = new HashMap<>();
 
     public static void main(String[] args) {
-        HashSet<String> procWords = new HashSet<>();
+        HashSet<String> processWords = new HashSet<>();
         int fileVowels = 0;
         int wordVowels;
 
@@ -22,14 +21,17 @@ public class Ex16 {
             for (char letter : word.toCharArray()) {
                 if (vowels.contains(letter)) {
                     wordVowels++;
+                    vowelAmount.put(letter, !vowelAmount.containsKey(letter) ? 1 : vowelAmount.get(letter) + 1);
                 }
+
             }
-            if (!procWords.contains(word)) {
-                procWords.add(word);
+            if (!processWords.contains(word)) {
+                processWords.add(word);
                 log.info(word + " has " + wordVowels + " vowel(s)");
             }
             fileVowels += wordVowels;
         }
         log.info("Total number vowels in the file are " + fileVowels);
+        log.info(vowelAmount.toString());
     }
 }
