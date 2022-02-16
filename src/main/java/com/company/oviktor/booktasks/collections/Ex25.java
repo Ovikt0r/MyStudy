ce958d30628f345c73425ccba402495d4d60c945
Ovikt0r <viktor.oblapenko@gmail.com>
2022-02-16 23:29:43 +0200

    Initial commit adding exercise Ex25

---
+++ src/main/java/com/company/oviktor/booktasks/collections/Ex25.java
@@ -0,0 +1,26 @@
+package com.company.oviktor.booktasks.collections;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.*;
+
+@Slf4j
+public class Ex25 {
+
+
+    public static void main(String[] args) {
+        Map<String, ArrayList<Integer>> wordCounter = new TreeMap<>();
+
+        Integer i = 0;
+        for (String word : new TextFile("text.txt", "\\W+")) {
+            ArrayList<Integer> wordList = wordCounter.get(word);
+            if (wordList == null) {
+                wordList = new ArrayList<>();
+                wordCounter.put(word, wordList);
+            }
+            wordList.add(++i);
+        }
+        ;
+        log.info(wordCounter.toString());
+    }
+}
