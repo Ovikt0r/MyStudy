d005a19c0bd24ecc99e83752ce8d9947c765e6e9
Ovikt0r <viktor.oblapenko@gmail.com>
2022-05-10 19:26:54 +0300

    Initial commit Ex10

--- src/main/java/com/company/oviktor/booktasks/contolingexecution/Ex10.java
+++ src/main/java/com/company/oviktor/booktasks/contolingexecution/Ex10.java
@@ -1,6 +1,12 @@
 package com.company.oviktor.booktasks.contolingexecution;
 
+import java.util.*;
+
 public class Ex10 {
+
+    // static Set<String> ts = new HashSet<>();
+    static SortedSet ss = new TreeSet();
+
 
     static int a (int i) {
         return i/1000;
@@ -17,24 +23,24 @@ public class Ex10 {
     static int com (int z, int y) {
         return (z * 10) + y;
     }
+
+
     static void productVampNumber (int i, int k, int l) {
-            if (i == k * l) {
-                System.out.println(i + "=" + k + "*" + l);
-            }
-        }
+        if (i == k * l)
+            ss.add(i + "=" + k + "*" + l);
+    }
+
     public static void main(String[] args) {
         for (int i = 1001; i < 9999; i++) {
-            productVampNumber(i, com(a(i), b(i)), com(c(i), d(i)));
-            productVampNumber(i, com(a(i), b(i)), com(d(i), c(i)));
-            productVampNumber(i, com(a(i), c(i)), com(b(i), d(i)));
-            productVampNumber(i, com(a(i), c(i)), com(d(i), b(i)));
-            productVampNumber(i, com(a(i), d(i)), com(b(i), c(i)));
-            productVampNumber(i, com(a(i), d(i)), com(c(i), b(i)));
+
             productVampNumber(i, com(b(i), a(i)), com(c(i), d(i)));
-            productVampNumber(i, com(b(i), a(i)), com(d(i), c(i)));
-            productVampNumber(i, com(b(i), c(i)), com(d(i), a(i)));
-            productVampNumber(i, com(b(i), d(i)), com(c(i), a(i)));
+            productVampNumber(i, com(a(i), d(i)), com(c(i), b(i)));
+            productVampNumber(i, com(c(i), a(i)), com(b(i), d(i)));
 
         }
+        // List<String> sortedNumbers = new ArrayList<String>(ts);
+        // Collections.sort(sortedNumbers);
+        System.out.println(ss);
+
     }
 }
