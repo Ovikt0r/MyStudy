854782268560c85648e3380b1f48a3ce3bec93df
Ovikt0r <viktor.oblapenko@gmail.com>
2022-05-17 23:39:49 +0300

    Initial commit Ex09

---
+++ src/main/java/com/company/oviktor/booktasks/errorhandling/Ex09.java
@@ -0,0 +1,42 @@
+package com.company.oviktor.booktasks.errorhandling;
+
+
+import lombok.extern.slf4j.Slf4j;
+
+class MyException extends Exception{
+    MyException(String s){
+        super(s);
+    }
+}
+class MyException2 extends Exception{
+    MyException2(String s){
+        super(s);
+    }
+}
+
+class MyException3 extends Exception{
+    MyException3(String s){
+        super(s);
+    }
+}
+
+@Slf4j
+public class Ex09 {
+    static void f(int x) throws MyException, MyException2, MyException3 {
+        if (x > 0) throw new MyException("MyException thrown cause x > 0");
+        if (x < 0) throw new MyException2("MyException2 thrown cause x < 0 ");
+        if (x == 20) throw new MyException3("MyException3 thrown cause x = 20 ");
+    }
+    public static void main(String[] args) {
+        try {
+            f(-1);
+            f(1);
+            f(20);
+        }
+        catch (Exception e) {
+            log.info("I caught all exceptions");
+            e.printStackTrace(System.out);
+        }
+
+    }
+}
