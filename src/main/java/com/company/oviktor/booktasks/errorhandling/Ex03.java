0494a2e8ba3284e3f5dbe8418b7d14652b757e70
Ovikt0r <viktor.oblapenko@gmail.com>
2022-05-17 20:56:12 +0300

    Initial commit Ex07

--- src/main/java/com/company/oviktor/booktasks/errorhandling/Ex03.java
+++ src/main/java/com/company/oviktor/booktasks/errorhandling/Ex03.java
@@ -2,8 +2,17 @@ package com.company.oviktor.booktasks.errorhandling;
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
 @Slf4j
 public class Ex03 {
+
+    static void logException(Exception e) {
+        StringWriter trace = new StringWriter();
+        e.printStackTrace(new PrintWriter(trace));
+        log.error(trace.toString());
+    }
     public static void main(String[] args) {
 
         try {
@@ -12,6 +21,7 @@ public class Ex03 {
             log.info(strings[10]);
         }
         catch (ArrayIndexOutOfBoundsException e) {
+            logException(e);
             log.info("exception that i've caught is: " + e);
         }
 
